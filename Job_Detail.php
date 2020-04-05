ea25beb501894827f99b8c504f8188970dd41f4f
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-05 10:41:41 -0500

    Update Job_Detail.php

--- Job_Detail.php
+++ Job_Detail.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR?xhtmll/DTD/xhtmll-strict.dtd">
 <!-- 
    | -Given job id, searches database for info related to that job. (SQL queries)
    | -Job creator can:
@@ -12,7 +12,7 @@
 	session_start();
 ?>
 
-<html>
+<html xmlns="http://ww.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <body>
 
 <!-- Gets job info from database -->
@@ -32,15 +32,15 @@
 	
 	//Add more labels
 	echo '<h2>Job Listing</h2>';
-	echo 'Job Title: '.$row['JobName'].'<br>';
-	echo 'Description: '.$row['Description'].'<br>';
-	echo 'Positions Available: '.$row['Number_available'].'<br>';
-	echo 'Qualifications: '.$row['Qualification'].'<br>';
-	echo 'SalaryRange: '.$row['SalaryRange'].'<br>';
-	echo 'Location: '.$row['Location'].'<br>';
-	echo 'Benefits: '.$row['Benefits'].'<br>';
-	echo 'Questions: '.$row['Questions'].'<br>';
-	echo '<br>';
+	echo 'Job Title: '.$row['JobName'].'<br/>';
+	echo 'Description: '.$row['Description'].'<br/>';
+	echo 'Positions Available: '.$row['Number_available'].'<br/>';
+	echo 'Qualifications: '.$row['Qualification'].'<br/>';
+	echo 'SalaryRange: '.$row['SalaryRange'].'<br/>';
+	echo 'Location: '.$row['Location'].'<br/>';
+	echo 'Benefits: '.$row['Benefits'].'<br/>';
+	echo 'Questions: '.$row['Questions'].'<br/>';
+	echo '<br/>';
 
 	//Job poster can edit job posting
 
@@ -58,35 +58,35 @@
 			// Poster can change JobName, Description, Open_Date,Close_Date, Number_available, Qualification, SalaryRange,JobType, Location, Benefits, Questions.
 			echo 'Hello Job Poster.';
 			echo '<form method="POST" action="">';
-			echo 'Job title:<input type="text" name="jobname"></input><br>';
-			echo 'Description:<input type="text" name="description"></input><br>';
-			echo 'Open date:<input type="text" name="opendate"></input><br>';
-			echo 'Close date:<input type="text" name="closedate"></input><br>';
-			echo 'Positions available:<input type="text" name="numberavailable"></input><br>';
-			echo 'Qualifications:<input type="text" name="qualification"></input><br>';
-			echo 'Salary range:<input type="text" name="salaryrange"></input><br>';
-			echo 'Job type:<input type="text" name="jobtype"></input><br>';
-			echo 'Location:<input type="text" name="location"></input><br>';
-			echo 'Benefits:<input type="text" name="benefits"></input><br>';
-			echo 'Questions:<input type="text" name="questions"></input><br>';
+			echo 'Job title:<input type="text" name="jobname"></input><br/>';
+			echo 'Description:<input type="text" name="description"></input><br/>';
+			echo 'Open date:<input type="text" name="opendate"></input><br/>';
+			echo 'Close date:<input type="text" name="closedate"></input><br/>';
+			echo 'Positions available:<input type="text" name="numberavailable"></input><br/>';
+			echo 'Qualifications:<input type="text" name="qualification"></input><br/>';
+			echo 'Salary range:<input type="text" name="salaryrange"></input><br/>';
+			echo 'Job type:<input type="text" name="jobtype"></input><br/>';
+			echo 'Location:<input type="text" name="location"></input><br/>';
+			echo 'Benefits:<input type="text" name="benefits"></input><br/>';
+			echo 'Questions:<input type="text" name="questions"></input><br/>';
 			echo '<input type="submit" name="updatePosting"></input>';
 			echo '</form>';
 		}
 		else{
-			echo '<form action="Job_Seeker_Register.php" type="POST">';
-			echo '<input type="submit" value="Create Jobseeker profile">';
+			echo '<form action="Job_Seeker_Register.php" type="POST"/>';
+			echo '<input type="submit" value="Create Jobseeker profile"/>';
 			echo '</form>';
 			echo '<form action="" type="POST">';
-			echo '<input type="submit" value="Apply">';
+			echo '<input type="submit" value="Apply"/>';
 			echo '</form>';
 		}
 	}
 	else{
 		echo '<form action="Job_Seeker_Register.php" type="POST">';
-		echo '<input type="submit" value="Create Jobseeker profile!">';
+		echo '<input type="submit" value="Create Jobseeker profile!"/>';
 		echo '</form>';
 		echo '<form action="" type="POST">';
-		echo '<input type="submit" value="Apply">';
+		echo '<input type="submit" value="Apply"/>';
 		echo '</form>';
 	}
 	
