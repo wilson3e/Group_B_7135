ff02da841af71e88e7493765ed61fd4e32836f77
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-05 10:37:49 -0500

    Update Job_Apps.php

--- Job_Apps.php
+++ Job_Apps.php
@@ -1,4 +1,5 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR?xhtmll/DTD/xhtmll-strict.dtd">
+
 <!--
    | -Page for jobseekers to view their applications
    | -Only reachable from user's profile when the user is logged in
@@ -9,36 +10,37 @@
 	session_start();
 ?>
 
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-</head>
-<body>
-    <h1>Current Applications </h1>
-    <?php
-    $getApplications = "SELECT JobName, Applied_date, Description, B.CompanyID
-            FROM application as A, job as B, jobseeker as C
-            WHERE C.JobSeekerID = A.JobseekerID AND A.JobID = B.JobID
-            GROUP BY A.ApplicationID
-            ORDER BY Close_date";
+<html xmlns="http://ww.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+    		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          	<link href="https://fonts.googleapis.com/css?family=Work+Sans: 30%"  />
+   		<title></title>
+	</head>
+	<body>
+    		<h1>Current Applications </h1>
+   		 <?php
+ 			$getApplications = "SELECT JobName, Applied_date, Description, B.CompanyID
+           		FROM application as A, job as B, jobseeker as C
+           		WHERE C.JobSeekerID = A.JobseekerID AND A.JobID = B.JobID
+            		GROUP BY A.ApplicationID
+            		ORDER BY Close_date";
 	
-	$result = query($getApplications,$db);
-	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	$count = mysqli_num_rows($result);
+			$result = query($getApplications,$db);
+			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+			$count = mysqli_num_rows($result);
 
-    if ($count > 0) {
-		echo "<table><tr><th>Job Title</th><th>Submission Date</th><th>Job Description</th><th>Company</th></tr>";
-        while($row = $result->fetch_assoc()) {
-			echo "<tr><td>".$row["JobName"]."</td><td>".$row["Applied_date"]."</td><td>".$row["Description"]."</td><td> ".$row["B.CompanyID"]."</td></tr>";
-        }
-        echo "</table>";
-    } 
-	else {
-		echo "0 results";
-    }
-	$db -> close();
-    ?>
+    			if ($count > 0) {
+				echo "<table><tr><th>Job Title</th><th>Submission Date</th><th>Job Description</th><th>Company</th></tr>";
+        			while($row = $result->fetch_assoc()) {
+				echo "<tr><td>".$row["JobName"]."</td><td>".$row["Applied_date"]."</td><td>".$row["Description"]."</td><td> ".$row["B.CompanyID"]."</td></tr>";
+        				}
+        			echo "</table>";
+   			} 
+			else {
+				echo "0 results";
+    			}
+			$db -> close();
+  	  	?>
 
     <!--Need to Add Application Numbering ie 1 to 50 && add a remove/delete button to each row-->
 
