131bafab4a5af7ad9b19ad1ca77c50cd7a214441
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-05 10:49:46 -0500

    Update Job_Posting.php

--- Job_Posting.php
+++ Job_Posting.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR?xhtmll/DTD/xhtmll-strict.dtd">
 
 <!--
    | -Page for recruiters to create new job postings
@@ -6,7 +6,7 @@
    | -Unfinished
 -->
 
-<html>
+<html xmlns="http://ww.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <body>
 
 <h1>Input Job Attributes </h1>
@@ -15,42 +15,42 @@
 -->
 <form action="">  
   <label for="JobName">Job Name:</label>
-  <input type="text" id="JobName" name="JobName" ><br><br>
+  <input type="text" id="JobName" name="JobName" /><br/><br/>
   
   <label for="Description">Job Description:</label>
-  <input type="text" id="Description" name="Description" ><br><br>
+  <input type="text" id="Description" name="Description" /><br/><br/>
   
   <label for="CompanyID">Company ID:</label>
-  <input type="text" id="CompanyID" name="CompanyID" ><br><br>  
+  <input type="text" id="CompanyID" name="CompanyID" /><br/><br/>  
   
   <label for="Open_Date">Open Date:</label>
-  <input type="text" id="Open_Date" name="Open_Date" ><br><br>
+  <input type="text" id="Open_Date" name="Open_Date" /><br/><br/>
   
   <label for="Close_Date">Close Date:</label>
-  <input type="text" id="Close_date" name="Close_date" ><br><br>
+  <input type="text" id="Close_date" name="Close_date" /><br/><br/>
   
   <label for="Number_available">Number of Openings:</label>
-  <input type="text" id="Number_available" name="Number_available" ><br><br>
+  <input type="text" id="Number_available" name="Number_available" /><br/><br/>
   
   <label for="Qualification">Qualification:</label>
-  <input type="text" id="Qualification" name="Qualification" ><br><br>
+  <input type="text" id="Qualification" name="Qualification" /><br/><br/>
   
   <label for="SalaryRange">Salary Range:</label>
-  <input type="text" id="SalaryRange" name="SalaryRange" ><br><br>
+  <input type="text" id="SalaryRange" name="SalaryRange" /><br/><br/>
   
   <label for="JobType">Job Type:</label>
-  <input type="text" id="JobType" name="JobType" ><br><br>
+  <input type="text" id="JobType" name="JobType" /><br/><br/>
   
   <label for="Location">Location:</label>
-  <input type="text" id="Location" name="Location" ><br><br>
+  <input type="text" id="Location" name="Location" /><br/><br/>
     
   <label for="Benefits">Benefits:</label>
   <input type="text" id="Benefits" name="Benefits" ><br><br>
   
   <label for="Questions">Questions:</label>
-  <input type="text" id="Questions" name="Questions" ><br><br>
+  <input type="text" id="Questions" name="Questions" /><br/><br/>
   
-  <input type="submit" value="Submit">
+  <input type="submit" value="Submit"/>
 </form>
 
 </body>
