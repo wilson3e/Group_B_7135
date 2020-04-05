b482556e4636fe862238685f392fff8434342698
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-05 10:23:59 -0500

    Update Search.php

--- search/Search.php
+++ search/Search.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR?xhtmll/DTD/xhtmll-strict.dtd">
 <?php
 	
 ?>
-
-<html lang="en">
+<html xmlns="http://ww.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8"/>
 		<title>Search Demo</title>
 	</head>
 	<body>
 		<form action="Search_Results.php" method="get">
-			<input type="text" name="search" id="search" placeholder="Text goes here"><br>
-			<input type="submit">
+			<input type="text" name="search" id="search" placeholder="Text goes here"/><br/>
+			<input type="submit"/>
 		</form>
 	</body>
 </html>
