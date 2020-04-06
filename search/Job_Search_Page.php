832900f2e5130b5f76649b418869f63ad81e4744
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-06 10:58:07 -0500

    Update Job_Search_Page.php

--- search/Job_Search_Page.php
+++ search/Job_Search_Page.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR?xhtmll/DTD/xhtmll-strict.dtd">
 
 <html xmlns="http://ww.w3.org/1999/xhtml" lang="en" xml:lang="en">
-
+  <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href="https://fonts.googleapis.com/css?family=Work+Sans: 30%"  />
+            <link type="text/css" rel="stylesheet" href="style.css"/>
+            <title>Job Search Page</title>
+        </head>
 <body>
 
 <h1>The input value attribute</h1>
